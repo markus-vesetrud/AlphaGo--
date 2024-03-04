c4f697fdc18bf768ad024fddf767be8fee4129ce
Markus Risa Vesetrud <markus.vesetrud@gmail.com>
2024-03-04 13:16:46 +0100

    Make first draft of game interface

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # AlphaGo--
+
+
+## TODO
+
+* Make board representation, update functions, and visualisation for both Hex and Nim (debug)
+* Make a general MCTS system with rollouts
+* 
---
+++ game_interface.py
@@ -0,0 +1,29 @@
+
+class GameInterface:
+
+    # def __init__(self) -> None:
+    #     pass
+
+    def is_final_state(self) -> int:
+        """
+        returns: -1 for loss, 0 for not final state, and 1 for win
+        """
+        pass
+
+
+    def get_legal_acions(self) -> list[bool]:
+        """
+        returns
+        """
+        pass
+
+
+    def get_state(self) -> tuple[list[int], bool]:
+        """
+        returns
+        """
+        pass
+
+
+    
+
