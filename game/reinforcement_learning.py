b66a9792abc028d1c0e14e63513b78e452ba9ea4
Simen <simeeid@stud.ntnu.no>
2024-04-05 11:11:05 +0200

    Registering multiple games and epochs

--- game/reinforcement_learning.py
+++ game/reinforcement_learning.py
@@ -31,50 +31,51 @@ num_epochs = 5
 
 # ------------------- Game -------------------
 
-# game: GameInterface = Nim([10,10], 4)
-game: GameInterface = Hex(board_size)
-board, black_to_play = game.get_state(False)
+for _ in range(5):
+    # game: GameInterface = Nim([10,10], 4)
+    game: GameInterface = Hex(board_size)
+    board, black_to_play = game.get_state(False)
 
-# game_states: list[GameInterface] = [deepcopy(game)]
+    # game_states: list[GameInterface] = [deepcopy(game)]
 
-root_node = MCTreeNode(None, -1, black_to_play, game.get_legal_acions(True), game.get_action_count())
+    root_node = MCTreeNode(None, -1, black_to_play, game.get_legal_acions(True), game.get_action_count())
 
-# print(board)
-input_board = board.copy()
+    # print(board)
+    # input_board = board.copy()
 
-while not game.is_final_state():
-    
-    mcts = MCTreeSearch(root_node, game, exploration_weight, random_policy=True)
+    while not game.is_final_state():
+        
+        mcts = MCTreeSearch(root_node, game, exploration_weight, random_policy=True)
 
-    mcts.UCTSearch(search_iterations)
-    best_action = mcts.root.best_action()
+        mcts.UCTSearch(search_iterations)
+        best_action = mcts.root.best_action()
 
-    # if mcts.root.black_to_play:
-    #     mcts.UCTSearch(search_iterations)
-    #     best_action = mcts.root.best_action()
-    # else:
-    #     best_action = int(input("Action: "))
-    
-    # with np.printoptions(precision=2, suppress=True):
-    #     print('best', best_action)     
-    #     print('total node searches:', mcts.root.total_visit_count)     
-    #     print(root_node.action_values)
+        # if mcts.root.black_to_play:
+        #     mcts.UCTSearch(search_iterations)
+        #     best_action = mcts.root.best_action()
+        # else:
+        #     best_action = int(input("Action: "))
+        
+        # with np.printoptions(precision=2, suppress=True):
+        #     print('best', best_action)     
+        #     print('total node searches:', mcts.root.total_visit_count)     
+        #     print(root_node.action_values)
 
-    # train_instance1 = root_node.action_values
+        # train_instance1 = root_node.action_values
 
-    current_game_state, current_black_to_play = game.get_state(False)
-    if(current_black_to_play):
-        game_states.append(deepcopy(current_game_state))
-        target_values.append(root_node.action_values)     
-    
-    root_node = mcts.root.get_child(best_action)
-    root_node.make_root()
+        current_game_state, current_black_to_play = game.get_state(False)
+        if(current_black_to_play):
+            game_states.append(deepcopy(current_game_state))
+            target_values.append(root_node.action_values)     
+        
+        root_node = mcts.root.get_child(best_action)
+        root_node.make_root()
 
-    game.perform_action(best_action)
-    # game_states.append(deepcopy(game))
-    # game.display_current_state()
+        game.perform_action(best_action)
+        # game_states.append(deepcopy(game))
+        # game.display_current_state()
 
-    # break
+        # break
 
 print("len board: ", len(game_states))
 print("len target: ", len(target_values))
@@ -98,7 +99,7 @@ game4.display_current_state() """
 # stop the file from running
 # exit()
 
-# ------------------- Neural network -------------------
+# ------------------- Convolutional neural network -------------------
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -132,8 +133,27 @@ for epoch in range(num_epochs):
             print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
                    .format(epoch+1, num_epochs, i+1, total_step, loss.item())) """
 
+total_step = len(game_states)
+for epoch in range(num_epochs):
+    for i, (game_state, target_value) in enumerate(zip(game_states, target_values)):
+        tensor_input_board = torch.tensor(game_state, dtype=torch.float32).to(device)
+        input_board = tensor_input_board.permute(2, 0, 1).unsqueeze(0).to(device)
 
-for i in range(len(game_states)):
+        tensor_target_value = torch.tensor(target_value, dtype=torch.float32).to(device)
+
+        outputs = model(input_board).to(device)
+        loss = criterion(outputs[0], tensor_target_value).to(device)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i+1) % 10 == 0:
+            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+""" for i in range(len(game_states)):
     tensor_input_board = torch.tensor(game_states[i], dtype=torch.float32).to(device)
     input_board = tensor_input_board.permute(2, 0, 1).unsqueeze(0).to(device)
 
@@ -145,7 +165,7 @@ for i in range(len(game_states)):
     # Backward and optimize
     optimizer.zero_grad()
     loss.backward()
-    optimizer.step()
+    optimizer.step() """
 
 # print("outputs after: ")
 # print(model(input_board.to(device)))
