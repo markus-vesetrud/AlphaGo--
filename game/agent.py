edca4281b69834595b8e9c4e5c9fa74dc2feae64
Markus Risa Vesetrud <markus.vesetrud@gmail.com>
2024-04-22 12:21:56 +0200

    adding topp
    Authored-by: Simen Eidal <simeeid@users.noreply.github.com>

---
+++ game/agent.py
@@ -0,0 +1,86 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from board_flipping_util import flip_board_perspective, flip_target_values_position
+
+class Agent():
+    def select_action(self, game_board: np.ndarray, play_as_black: bool, legal_actions: list[int]) -> int:
+        """
+        Returns one of the integers in legal_actions
+        """
+
+
+class RandomAgent(Agent):
+    def select_action(self, game_board: np.ndarray, play_as_black: bool, legal_actions: list[int]) -> int:
+        """
+        Returns one of the integers in legal_actions
+        """
+        return np.random.choice(legal_actions)
+
+
+class PolicyAgent(Agent):
+    def __init__(self, board_size, model: nn.Module, epsilon: float) -> None:
+        super().__init__()
+
+        self.board_size = board_size
+        self.model = model
+        self.epsilon = epsilon
+
+
+    def __rescale_prediction(self, prediction: np.ndarray, legal_actions: list[int]) -> np.ndarray:
+        """
+        Rescales the prediction so that the entries with indices not specified by legal_actions are 0,
+        but the sum of prediction are still 1.
+        """
+        legal_mask = np.isin(np.arange(self.board_size**2), legal_actions)
+
+        prediction *= legal_mask
+
+        return prediction / np.sum(prediction)
+
+
+    def select_action(self, game_board: np.ndarray, play_as_black: bool, legal_actions: list[int], verbose = False) -> int:
+        """
+        Runs the agent model on the game_board, and with probability epsilon selects a random action, and 
+        with probability 1-epsilon returns the best action.
+        """
+
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(legal_actions)
+
+        # If it should play as red, then flip the prespective
+        # This makes a new board where black is in the same situation as red was
+        # Then the model finds (hopefully) good moves in this position
+        if not play_as_black:
+            game_board = flip_board_perspective(self.board_size, game_board)
+        
+        # Convert the board to the format expected by the model
+        # (A torch float32 Tensor with an added dimension for the batch size and the last dimension moved to the front)
+        game_board = torch.from_numpy(game_board).float()
+        game_board = game_board.unsqueeze(0).permute(0, 3, 1, 2)
+
+        self.model.eval()
+        with torch.no_grad():
+            prediction = self.model(game_board)
+        
+        # Extract the prediction
+        prediction = np.array(prediction[0,:])
+
+        # If it played as red, then the prediction needs to be flipped back again
+        # After this, the prediction will correspond to the given board, and not 
+        # the board the model saw
+        if not play_as_black:
+            prediction = flip_target_values_position(self.board_size, prediction)
+
+        # Set illegal moves to be zero, important that this is done after the flipping above
+        prediction = self.__rescale_prediction(prediction, legal_actions)
+
+        if verbose:
+            print(prediction.reshape((self.board_size, self.board_size)))
+
+
+        # Return the best prediction
+        return prediction.argmax()
+    
+        # Return a prediction proportional to the probability of that prediction
+        # return np.random.choice(np.arange(prediction.shape[0]), p=prediction)
---
+++ game/topp.py
@@ -0,0 +1,87 @@
+import numpy as np
+from agent import Agent
+from game_interface import GameInterface
+from hex import Hex
+
+class TOPP():
+    """
+    A class to represent the Tournament of Progressive Policies (TOPP).
+    Each agent plays against each other agent for a number of games.
+    """
+    def __init__(self, number_of_games: int, board_size: int, agents: list[Agent]) -> None:
+        self.number_of_games = number_of_games
+        self.board_size = board_size
+        self.agents = agents
+        self.scores = np.zeros((len(agents), len(agents)))
+
+    def play_tournament(self) -> np.ndarray:
+        """
+        Play the tournament of progressive policies.
+        Each agent plays against each other agent for a number of games.
+        The number of wins is recorded in a matrix in percentage.
+        The upper triangular part of the matrix is filled with the win percentage
+        of the row agent playing as black against the column agent playing as red.
+        """
+        for i in range(len(self.agents)):
+            for j in range(i, len(self.agents)):
+                if i != j:
+                    agent1 = self.agents[i]
+                    agent2 = self.agents[j]
+
+                    for _ in range(self.number_of_games):
+                        game: GameInterface = Hex(self.board_size)
+
+                        # play the game
+                        while not game.is_final_state():
+                            board, black_to_play = game.get_state(False)
+                            legal_actions = game.get_legal_actions()
+
+                            if black_to_play:
+                                action = agent1.select_action(board, black_to_play, legal_actions)
+                            else:
+                                action = agent2.select_action(board, black_to_play, legal_actions)
+
+                            game.perform_action(action)
+                        
+                        # update scores
+                        self.scores[i, j] += game.get_winner()
+                        
+        return self.scores / self.number_of_games
+
+    def visualize_results(self):
+        """
+        Visualize the results of the tournament.
+        Visualization is on the form black agent vs red agent: win percentage of black agent.
+        """
+        print("Black agent\t| Red agent\t| Win percentage of black")
+        for i in range(len(self.agents)):
+            print("-------------------------------------------------")
+            for j in range(len(self.agents)):
+                if i != j:
+                    print(f"Agent {i}\t\t| Agent {j}\t| {(self.scores[i, j] / self.number_of_games):.2f}%")
+
+# class TestTOPP():
+#     def __init__(self) -> None:
+#         self.scores = np.zeros((3, 3))
+#         self.number = 0
+
+#     def test_scores(self, k):
+#         self.scores[0, 1] += k
+#         self.number += 1
+
+#     def visualize_results(self):
+#         print("Black agent\t| Red agent\t| Win percentage of black")
+#         for i in range(3):
+#             print("-------------------------------------------------")
+#             for j in range(3):
+#                 if i != j:
+#                     print(f"Agent {i}\t\t| Agent {j}\t| {(self.scores[i, j] / self.number):.2f}%")
+
+# if __name__ == "__main__":
+#     test = TestTOPP()
+#     test.test_scores(1)
+#     test.test_scores(0)
+#     test.test_scores(1)
+
+#     test.visualize_results()
+        
